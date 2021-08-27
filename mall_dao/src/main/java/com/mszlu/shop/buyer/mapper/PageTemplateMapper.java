b4165b4fe7584382b2ba60870118c80b54b14bab
于家勇 <1015086215@qq.com>
2021-08-27 21:18:35 +0800

    Revert "首页装修dubbo服务"
    This reverts commit b5addcac

--- mall_dao/src/main/java/com/mszlu/shop/buyer/mapper/PageTemplateMapper.java
+++
@@ -1,7 +0,0 @@
-package com.mszlu.shop.buyer.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.mszlu.shop.model.buyer.pojo.PageTemplate;
-
-public interface PageTemplateMapper extends BaseMapper<PageTemplate> {
-}
--- mall_dao/src/main/java/com/mszlu/shop/buyer/mapper/TemplateDetailMapper.java
+++
@@ -1,9 +0,0 @@
-package com.mszlu.shop.buyer.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.mszlu.shop.model.buyer.pojo.TemplateDetail;
-
-public interface TemplateDetailMapper extends BaseMapper<TemplateDetail> {
-
-
-}
